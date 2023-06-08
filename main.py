ca3aaaf51a5fb5e32bbccc48cb10028b924b8ed4
Fadeke <fadegboye59@gmail.com>
2023-06-08 16:58:00 +0100

    added unit test

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ print("Hello World")
 
 print("created a new update")
 
+def addition(num1, num2):
+    return num1 + num2
+
+
+def substraction(num1, num2):
+    return num1 - num2
 
 #added a cpomment file
